0b01e135cb2450fdc7cad56a46abdd0648e462d8
strangehumano2005 <112585795+strangehumano2005@users.noreply.github.com>
2022-09-21 13:32:50 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    if (input.temperature() == 10) {
+        basic.showString(" \"¡Vamos a esquiar!\"")
+    } else if (input.temperature() == 20) {
+        basic.showString("\"¡Hágamos un picnic!\"")
+    } else if (input.temperature() == 30) {
+        basic.showString("\"¡Vamos a la playa!\"")
+    } else if (input.temperature() == 40) {
+        basic.showString("\"Mejor no salgas\"")
+    }
 })
